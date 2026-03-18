752eaef67d56dd2a035bb557f3acefc325ce2a0d
Luis Alberto <106775678+luis-telesforo@users.noreply.github.com>
2026-03-17 21:39:22 -0600

    documented SimpleDateFormatter

--- rewards/src/main/java/datetime/SimpleDateFormatter.java
+++ rewards/src/main/java/datetime/SimpleDateFormatter.java
@@ -6,20 +6,43 @@ import java.time.format.DateTimeFormatter;
 
 import static java.time.LocalDate.now;
 import static java.time.ZoneId.of;
-import static java.time.format.DateTimeFormatter.ISO_DATE;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
 
+/**
+ * Centralizes the use of {@link DateTimeFormatter#ISO_LOCAL_DATE},
+ * to simplify further refactoring of date format if needed.
+ */
 public class SimpleDateFormatter {
-    public static final ZoneId UTC_ZONE_ID = of("UTC+00");
+    /**
+     * This application assumes the date is the one from {@code UTC+00}.
+     */
+    private static final ZoneId UTC_ZONE_ID = of("UTC+00");
 
+    /**
+     * @return The current date in  {@code UTC+00}.
+     */
     public static LocalDate today() {
        return now(UTC_ZONE_ID);
     }
 
+    /**
+     * Parses the given {@link String} using {@link DateTimeFormatter#ISO_LOCAL_DATE}.
+     * @param date The date to be parsed
+     * @return The {@link LocalDate} represented by the given date if it is in the right format.
+     * @see DateTimeFormatter#parse
+     */
     public static LocalDate parseDate(String date) {
-        return ISO_DATE.parse(date, LocalDate::from);
+        return ISO_LOCAL_DATE.parse(date, LocalDate::from);
     }
 
+    /**
+     * Formats the given {@link LocalDate} in the {@code ISO-8601} format.
+     * @param date The date to be formatted.
+     * @return The formatted date.
+     * @see <a href="https://es.wikipedia.org/wiki/ISO_8601">ISO-8601 definition</a>
+     * @see DateTimeFormatter#format
+     */
     public static String formatDate(LocalDate date) {
-        return ISO_DATE.format(date);
+        return ISO_LOCAL_DATE.format(date);
     }
 }
--- rewards/src/test/java/datetime/SimpleDateFormatterTest.java
+++ rewards/src/test/java/datetime/SimpleDateFormatterTest.java
@@ -1,31 +1,67 @@
 package datetime;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-import static datetime.SimpleDateFormatter.parseDate;
-import static datetime.SimpleDateFormatter.today;
+import static datetime.SimpleDateFormatter.*;
 import static java.time.LocalDate.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+/**
+ * Tests for {@link SimpleDateFormatter}
+ */
+@DisplayName("SimpleDateFormatter")
 class SimpleDateFormatterTest {
 
     @Test
-    void parseDateTest() {
+    @DisplayName("Parsing dates.")
+    void parseDatePositiveTest() {
         assertEquals(parseDate("1920-01-03"), of(1920, 1, 3));
     }
+
     @Test
-    void parseDateStrictTest() {
+    @DisplayName("Parsing requires 2 digits for months. ")
+    void parseDateMonthTest() {
         DateTimeParseException dateTimeParseException =
                 assertThrows(DateTimeParseException.class, () -> parseDate("1920-1-03"));
         assertEquals(5, dateTimeParseException.getErrorIndex());
     }
 
     @Test
-    void formatDate() {
+    @DisplayName("Parsing requires 2 digits for months. ")
+    void parseDateDaysTest() {
+        DateTimeParseException dateTimeParseException =
+                assertThrows(DateTimeParseException.class, () -> parseDate("1920-01-3"));
+        assertEquals(8, dateTimeParseException.getErrorIndex());
+    }
+
+    @Test
+    @DisplayName("Parsing requires 4 digits for years. ")
+    void parseDateYearsTest() {
+        DateTimeParseException dateTimeParseException =
+                assertThrows(DateTimeParseException.class, () -> parseDate("99-01-03"));
+        assertEquals(0, dateTimeParseException.getErrorIndex());
+    }
+
+    @Test
+    void parseDateStrictMonthTest() {
+        DateTimeParseException dateTimeParseException =
+                assertThrows(DateTimeParseException.class, () -> parseDate("1920-13-03"));
+        assertEquals("Text '1920-13-03' could not be parsed: Invalid value for MonthOfYear (valid values 1 - 12): 13", dateTimeParseException.getMessage());
+    }
+
+    @Test
+    void parseDateStrictDaysTest() {
+        DateTimeParseException dateTimeParseException =
+                assertThrows(DateTimeParseException.class, () -> parseDate("1920-01-32"));
+        assertEquals("Text '1920-01-32' could not be parsed: Invalid value for DayOfMonth (valid values 1 - 28/31): 32", dateTimeParseException.getMessage());
+    }
+
+        @Test
+    void formatDateTest() {
+        assertEquals("1920-01-03", formatDate(of(1920, 1, 3)));
     }
 }
